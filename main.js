c6df11d4e4021b6a76f890c437e21970ac1a9dc1
Asquel09 <166029157+Asquel09@users.noreply.github.com>
2024-09-19 14:04:14 +0000

    lipe

---
+++ main.js
@@ -0,0 +1,10 @@
+const botoes = document.querySelectorAll(".botao");
+const textos = document.querySelectorAll(".abas-conteudo");
+for(let i = 0; i < botoes.length; i++){
+    botoes[i].onclick = function (){
+        for(let j = 0; j < botoes.lenght; j++){
+            botoes[j].classList.remove ("ativo");
+            textos[j].classList.remove ("ativo");
+        }
+    } 
+}
